5811f6ef8872ab78aa59c54b84eb74374607a5bc
panda <33819821+godlike66@users.noreply.github.com>
2019-11-29 16:56:33 +0800

    Update FormVaild.js
    更新

--- FormVaild.js
+++ FormVaild.js
@@ -60,3 +60,23 @@ $.fn.numedot = function () {
         }
     }).css("ime-mode", "disabled"); //CSS设置输入法不可用      
 }
+//限制文本框只能输入正整数
+function numInteger(val) {
+    if (val.length == 1) {
+        val = val.replace(/[^1-9]/g, '');
+    } else {
+        val = val.replace(/\D/g, '');
+    }
+    return val;
+}
+//限制文本框只能输入整数或小数点且只保留2位小数
+function numDecimal(val) {
+    val = val.replace(/[^\d.]/g, "");  //清除“数字”和“.”以外的字符   
+    val = val.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的   
+    val = val.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
+    val = val.replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');//只能输入两个小数   
+    if (val.indexOf(".") < 0 && val != "") {
+        val = parseFloat(val);
+    }
+    return val;
+}
